11a37c36d90b43c4aea7b5bf700b8446f5a2265a
hasibferdous <hasibferdous.ict@gmail.com>
2024-03-04 00:21:46 +0600

    swiper silder added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import { Carousel } from 'react-responsive-carousel';
 
 
-import img1 from "../../../assets/1.jpg"
+// import img1 from "../../../assets/1.jpg"
 import img2 from "../../../assets/2.jpg"
 import img3 from "../../../assets/3.jpg"
 import img4 from "../../../assets/4.jpg"
@@ -12,9 +12,9 @@ const Banner = () => {
     return (
         <div className="">
             <Carousel>
-                <div>
+                {/* <div>
                     <img src={img1} />                    
-                </div>
+                </div> */}
                 <div>
                     <img src={img2} />                  
                 </div>
