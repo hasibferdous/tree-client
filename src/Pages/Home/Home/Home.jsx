038d1ac3b1cb89b123d747db812fcec8a8757cbd
hasibferdous <hasibferdous.ict@gmail.com>
2024-03-16 03:04:59 +0600

    review section updated

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../Banner/Banner";
 import BestSelling from "../BestSelling/BestSelling";
 import Category from "../Category/Category";
 import Featured from "../Featured/Featured";
+import Testimonials from "../Testimonials/Testimonials";
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <Category></Category>
             <BestSelling></BestSelling>
             <Featured></Featured>
+            <Testimonials></Testimonials>
         </div>
     );
 };
--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Navigation } from 'swiper/modules';
+
+import '@smastrom/react-rating/style.css'
+import { Rating } from '@smastrom/react-rating';
+import SectionTitle from '../../../components/SectionTilte/SectionTitle';
 
 const Testimonials = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect( () => {
+        fetch('reviews.json')
+            .then(res=> res.json())
+            .then(data => setReviews(data))
+    }, [])
+
     return (
         <section>
-            <SectionTitle subHeading="Look what's our Clients are saying" heading="Reviews"></SectionTitle>
-        
+            <SectionTitle subHeading="Look what our Clients say" heading="Reviews"></SectionTitle>
+
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                {
+                    reviews.map(review => <SwiperSlide
+                        key={review.id}
+                    >
+                        <div className='flex flex-col items-center mx-24 my-16'>
+                            <Rating
+                                style={{ maxWidth: 180 }}
+                                value={review.rating}
+                                readOnly
+                            />
+                            <p className='py-8'>{review.comment}</p>
+                            <h3 className='text-2xl text-orange-400'>{review.name}</h3>
+                        </div>
+                    </SwiperSlide>)
+                }
+            </Swiper>
         </section>
     );
 };
