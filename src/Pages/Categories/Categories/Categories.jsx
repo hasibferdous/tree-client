2a10de8f64b9800aad685a663e01291ed8aeae5a
hasibferdous <hasibferdous.ict@gmail.com>
2024-03-17 23:35:52 +0600

    React Helmet added

--- src/Pages/Categories/Categories/Categories.jsx
+++ src/Pages/Categories/Categories/Categories.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import { Helmet } from 'react-helmet-async';
 const Categories = () => {
     return (
         <div>
+            <Helmet>
+                <title>Tree Emporium | Categories</title>
+            </Helmet>
             <h1>Categories</h1>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,13 @@ import {
 } from "react-router-dom";
 import { router } from './Routes/Routes';
 
+import { HelmetProvider } from 'react-helmet-async';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='max-w-screen-xl mx-auto'></div>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <div className='max-w-screen-xl mx-auto'></div>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   </React.StrictMode>
 )
