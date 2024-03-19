b50814def01fa21e2cf3c07426dc3724f6a128e5
hasibferdous <hasibferdous.ict@gmail.com>
2024-03-19 23:03:23 +0600

    cover part added

--- src/Pages/Categories/Categories/Categories.jsx
+++ src/Pages/Categories/Categories/Categories.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
 
 const Categories = () => {
     return (
@@ -7,6 +8,7 @@ const Categories = () => {
             <Helmet>
                 <title>Tree Emporium | Categories</title>
             </Helmet>
+            <Cover></Cover>
             <h1>Categories</h1>
         </div>
     );
